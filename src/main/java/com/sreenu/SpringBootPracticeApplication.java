91d2920f414443eb6d650f74df202ee2578c8d9e
legendcnu <sreenu8400@gmail.com>
2021-11-29 12:49:39 +0530

    Revert "Added some comment"
    This reverts commit 1d75dee11942a1e714dc969f5c690e5f5b4d0f43.

--- src/main/java/com/sreenu/SpringBootPracticeApplication.java
+++ src/main/java/com/sreenu/SpringBootPracticeApplication.java
@@ -2,7 +2,7 @@ package com.sreenu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//some comment
+//
 @SpringBootApplication
 public class SpringBootPracticeApplication {
 
