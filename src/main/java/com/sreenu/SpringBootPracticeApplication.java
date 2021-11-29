37425c428dd795f396ece6f2c15184bcbc911aca
legendcnu <sreenu8400@gmail.com>
2021-11-29 12:12:22 +0530

    Second commit

--- src/main/java/com/sreenu/SpringBootPracticeApplication.java
+++ src/main/java/com/sreenu/SpringBootPracticeApplication.java
@@ -2,7 +2,7 @@ package com.sreenu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//
 @SpringBootApplication
 public class SpringBootPracticeApplication {
 
